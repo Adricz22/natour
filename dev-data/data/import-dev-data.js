b5d41a6c2b4da022d1524403f2b2587e5fad0366
Adrián Chávez <adri-142@hotmail.com>
2022-10-24 10:26:53 -0300

    final

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
+
+dotenv.config({ path: './config.env' });
+
+// CONECTA BD
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('Data succesfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit(); // para finalizar la funcion
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data succesfully delete!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
+// esta propiedad devuelve una matriz que contiene los argumentos pasados al proceso cuando se ejecuta en la línea de comandos.
+// el primer elemento es la ruta de ejecución del proceso y el segundo elemento es la ruta del archivo js.
